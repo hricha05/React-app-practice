c5114a468df591229a2a5f4d926b2cfc70b1996d
Harun Richards <harunrich@googlemail.com>
2018-01-17 10:11:08 +0000

    fixed folder structure/playing around with components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+
+// class Welcome extends React.Component {
+//     render() {
+//         return <h1>Hello World, {this.props.name}</h1>
+//     }
+// }
+
+// function Welcome (props) {
+//     return <h1>Hello, {props.name}</h1>
+// }
+
+// function App() {
+//     return (
+//         <div>
+//             <Welcome name="Amira"/>
+//             <Welcome name="Alika"/>
+//             <Welcome name="Alannah"/>
+//         </div>
+//     )
+// }
+
+// const element = <Welcome name ="Sara" />;
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
