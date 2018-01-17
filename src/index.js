d6e4f2a23e632fae9fdb8d0e83b677b83b4a38c6
Harun Richards <harunrich@googlemail.com>
2018-01-17 14:58:35 +0000

    re-structured clock component into a function

--- src/index.js
+++ src/index.js
@@ -4,27 +4,6 @@ import ReactDOM from 'react-dom';
 import Clock from './components/Clock/Clock';
 import registerServiceWorker from './registerServiceWorker';
 
-// class Welcome extends React.Component {
-//     render() {
-//         return <h1>Hello World, {this.props.name}</h1>
-//     }
-// }
-
-// function Welcome (props) {
-//     return <h1>Hello, {props.name}</h1>
-// }
-
-// function App() {
-//     return (
-//         <div>
-//             <Welcome name="Amira"/>
-//             <Welcome name="Alika"/>
-//             <Welcome name="Alannah"/>
-//         </div>
-//     )
-// }
-
-// const element = <Welcome name ="Sara" />;
 function tick() {
     ReactDOM.render(
     <Clock date = {new Date()}/>, 
