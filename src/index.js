bd4b91a31acc258fc2ec686a8b4d3af6ba073806
Harun Richards <harunrich@googlemail.com>
2018-01-19 10:13:16 +0000

    improved component logic/added styles

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Clock from './components/Clock/Clock';
+import Toggle from './components/Button/button'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
     <Clock />, 
+    document.getElementById('root')
+);
+
+ReactDOM.render(
+    <Toggle />, 
     document.getElementById('root')
 );
 registerServiceWorker();
