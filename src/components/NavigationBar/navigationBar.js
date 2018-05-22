ec3bb34c7694d7999f4edf324c87edd2e2d721c0
Harun Richards <Harun.Richards@SBFC.local>
2018-05-22 08:14:41 +0100

    added NavigationBar component

---
+++ src/components/NavigationBar/navigationBar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class NavigationBar extends Component {
+    render() {
+        return (
+            <div className="NavigationBar">
+                <ul className="">
+                    <li id=""><a href="#"></a></li>
+                    <li id=""><a href="#"></a></li>
+                    <li id=""><a href="#"></a></li> 
+                </ul>
+            </div>
+        )
+    }
+}
